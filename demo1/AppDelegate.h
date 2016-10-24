add064c94514f634a62ef63dfb6ecb20415bfdc8
spacetime <jingweiming-sp@outlook.com>
2016-10-24 17:49:11 +0800

    Initial Commit

---
+++ demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo1
+//
+//  Created by spacetime on 10/24/16.
+//  Copyright © 2016 spacetime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
